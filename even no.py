a4f1ee3ff62aebe1cdf4531771725b2fd002950f
tempshot <66846262+tempshot@users.noreply.github.com>
2022-03-03 15:59:37 +0530

    Create even no.py

---
+++ even no.py
@@ -0,0 +1,9 @@
+# n = int(input("Enter n: "))
+# m = int(input("Enter m: "))
+# print("Divisible num")
+# for i in range(1,n):
+#     if i%m == 0:
+#         if i%2 == 0:
+#             print(i, " even", end=", ")
+#         else:
+#             print(i, " odd", end=", ")
