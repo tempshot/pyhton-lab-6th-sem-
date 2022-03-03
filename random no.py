68d48d96e929bc143b20ab206219c2e17b8d0222
tempshot <66846262+tempshot@users.noreply.github.com>
2022-03-03 16:02:54 +0530

    Update random no.py

--- random no.py
+++ random no.py
@@ -1,7 +1,7 @@
 import random
-# def fun(n):
-#     r = random.randint(10*(n-1),(10*n - 1))
-#     return r
-# 10*(n-1),(10*n - 1)
-# n = int(input("Enter n: "))
-# print(fun(n))
+def fun(n):
+    r = random.randint(10*(n-1),(10*n - 1))
+    return r
+10*(n-1),(10*n - 1)
+n = int(input("Enter n: "))
+print(fun(n))
