9444b08cba0c2d154f04bc6930fe48a837fdc98b
tempshot <66846262+tempshot@users.noreply.github.com>
2022-03-03 16:00:54 +0530

    Create largest no.py

---
+++ largest no.py
@@ -0,0 +1,7 @@
+n = int(input("Enter size of list: "))
+# l = []
+# print("Enter elements: ")
+# for i in range(1,n+1):
+#     m = int(input())
+#     l.append(m)
+# print("Max element is: ", max(l))
